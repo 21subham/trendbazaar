0b8813c151e729ab52c67ab878e0643e5adde2d3
21subham <subhamrajbhandari@gmail.com>
2024-12-30 13:25:42 +0545

    delete unused

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,8 +2,6 @@ import { type SchemaTypeDefinition } from "sanity";
 
 import { blockContentType } from "./blockContentType";
 import { categoryType } from "./categoryType";
-import { postType } from "./postType";
-import { authorType } from "./authorType";
 import { productType } from "./productType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
