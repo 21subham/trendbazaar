0809cb7ec921e59763894a6705ed13ce9b1bc605
21subham <subhamrajbhandari@gmail.com>
2024-12-30 10:15:47 +0545

    schema extraction with typegen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,7 @@ function Header() {
   return (
     <header className="flex flex-wrap justify-between items-center px-4 py-2">
       {/* top row */}
-      <div className="flex w-full flex-wrap justify-between items-center">
+      <div className="flex w-full flex-wrap justify-between items-center ">
         <Link
           href="/"
           className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
@@ -46,10 +46,10 @@ function Header() {
         </Form>
 
         {/* basket */}
-        <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 :flex-none">
+        <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 md:flex-none">
           <Link
             href="/basket"
-            className="flex-1 relative flex justify0center sm:justify:start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
             <TrolleyIcon className="w-6 h-6" />
             {/* Span item count pachi ko lagi */}
@@ -61,7 +61,7 @@ function Header() {
             <SignedIn>
               <Link
                 href="/orders"
-                className="flex-1 relative flex justify-center sm:justify-start sm:flex-name items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               >
                 <PackageIcon className="w-6 h-6" />
                 <span>My Orders</span>
