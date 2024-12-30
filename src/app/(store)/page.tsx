54d261a7476ef6b308875b18f642adbe5a3ffe5b
21subham <subhamrajbhandari@gmail.com>
2024-12-30 15:14:45 +0545

    queries + typegen

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
+
   return (
     <div>
       <h1> hello world 123</h1>
+      {/* render products*/}
       <Button>Click me</Button>
     </div>
   );
